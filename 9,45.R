6f0be97933e3e1f9fa54879414482184515debc3
TURNO <TURNO@ecinut.escuelaing.edu.co>
2019-11-08 11:30:15 -0500

    Actializacion de problemas

---
+++ 9,45.R
@@ -0,0 +1,27 @@
+z=2.306
+n<-9
+valor<-1000
+resultadoInferior<- 1:valor
+resultadoSuperior<- 1:valor
+contador<-0
+for( i in 1:valor){
+  valores<- 1:n
+  valores1<- 1:n
+  for(x in 1:n){
+    valores1[x]<- runif(1, min=0, max=100)
+    valores[x]<- runif(1, min=0, max=100)
+  }
+  finValor <- valores- valores1
+  s<- sd(finValor)
+  mu<- mean(finValor)
+  resultadoInferior[i]= mu-(z*(s/sqrt(n)))
+  resultadoSuperior[i]= mu+(z*(s/sqrt(n)))
+  if (resultadoInferior[x]<0){
+    if (resultadoSuperior[x]>0){
+      contador<- contador+1
+    }
+  }
+}
+contador
+valores
+
