48bb900ea8311e9a42921e65613cce878800fb5d
Jose Luis Gomez Camacho <josemania10@gmail.com>
2019-11-11 18:06:52 -0500

    parte de fredy terminada

--- 9,39.R
+++ 9,39.R
@@ -1,37 +1,33 @@
-  z<-5.305*2.763
-  contadorI<-0
-  contadorS<-0
-  valor<-1000
-  xbarra1<-1:valor
-  xbarra2<-1:valor
-  sigma1<- 1:valor
-  sigma2<- 1:valor
-  n1<- 1:valor
-  n2<- 1:valor
-  resultadoInferior<- 1:valor
-  resultadoSuperior<- 1:valor
-  for(x in 1:valor){
-    xbarra1[x]<- runif(1, min=34, max=134)
-    xbarra2[x]<- runif(1, min=27, max=127)
-    sigma1[x]<- runif(1, min=3, max=5)
-    sigma2[x]<- runif(1, min=5, max=7)
-    n1[x]<- runif(1, min=2, max=22)
-    n2[x]<- runif(1, min=8, max=28)
-    xbarra<- xbarra1[x]-xbarra2[x]
-    divicion1<- 1/n1[x]
-    divicion2<- 1/n2[x]
-    suma<-divicion1+divicion2
-    raiz<- sqrt(suma)
-    resultadoInferior[x]<- xbarra-(z*raiz)
-    resultadoSuperior[x]<- xbarra+(z*raiz)
-    if(resultadoInferior[x]>1.537){
-      contadorI<- contadorI+1
-    }
-    if(resultadoSuperior[x]<12.463){
-      contadorS<- contadorS+1
+z<- 2.763*5.305
+valor<-1000
+xbarra1<-1:valor
+xbarra2<-1:valor
+sigma1<- 1:valor
+sigma2<- 1:valor
+n1<- 1:valor
+n2<- 1:valor
+resultadoInferior<- 1:valor
+resultadoSuperior<- 1:valor
+contador<-0
+for(x in 1:valor){
+  xbarra1[x]<- runif(1, min=6, max=8)
+  xbarra2[x]<- runif(1, min=6, max=8)
+  sigma1[x]<- runif(1, min=1, max=2)
+  sigma2[x]<- runif(1, min=1, max=2)
+  n1[x]<- runif(1, min=2, max=22)
+  n2[x]<- runif(1, min=8, max=28)
+  xbarra<- xbarra1[x]-xbarra2[x]
+  divicion1<- (sigma1[x]^2)/n1[x]
+  divicion2<- (sigma2[x]^2)/n2[x]
+  suma<-divicion1+divicion2
+  raiz<- sqrt(suma)
+  resultadoInferior[x]<- xbarra-(z*raiz)
+  resultadoSuperior[x]<- xbarra+(z*raiz)
+  if (resultadoInferior[x]<0){
+    if (resultadoSuperior[x]>0){
+      contador<- contador+1
     }
   }
-  contadorS
-  contadorI
-  contador<- contadorI+contadorS
-  contador
+}
+tabla <- data.frame(cbind(resultadoInferior, resultadoSuperior))
+contador
